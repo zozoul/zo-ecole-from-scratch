4683e593ec7786ea1b36bbf936be50e2753d2336
Herizo Rasolomampionona <hrz.rasolo@gmail.com>
2021-08-20 18:18:52 +0300

    2021-08-20 [EVOL] (Zo) Organisation des dossiers

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'fc04d871ec7c7915045d716942c06c714d901a21',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'fc04d871ec7c7915045d716942c06c714d901a21',
+            'dev_requirement' => false,
+        ),
+        'altorouter/altorouter' => array(
+            'pretty_version' => '2.0.1',
+            'version' => '2.0.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../altorouter/altorouter',
+            'aliases' => array(),
+            'reference' => '127f6e96998708a31ef32252985bea82e3b03888',
+            'dev_requirement' => false,
+        ),
+    ),
+);
